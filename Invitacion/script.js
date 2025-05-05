c8a93d47c30fce794fd929cfbcceee7d13f9dd5b
danisfruu <floresalbarranalondradaniela1@gmail.com>
2025-05-04 20:58:36 -0600

    Invitacion

---
+++ Invitacion/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const left = document.querySelector('.character.left');
+    const right = document.querySelector('.character.right');
+  
+    left.style.animation = 'moveInLeft 2s forwards';
+    right.style.animation = 'moveInRight 2s forwards';
+  });
+  
